615ac56edb21eb1815937d71d1eae9fac74e9197
CombustibleToast <34487106+CombustibleToast@users.noreply.github.com>
2026-04-26 22:56:00 -0400

    implement ball adding and they appear visually.

--- submission/balls.js
+++ submission/balls.js
@@ -30,4 +30,66 @@
 		adSuccess();
 	});
 
+	document.getElementById("debugPause").addEventListener('click', () => window.top.postMessage({type: "pause"}, '*'))
+
+	console.log(window.innerWidth);
+	console.log(window.innerHeight);
+
+	// Add deploy ball listener
+	const ballButton = document.getElementById("debugAddBall");
+	ballButton.addEventListener("click", () => {
+		addBall(Math.random() * window.innerWidth, Math.random() * window.innerHeight);
+	})
+
+	// Begin tick loop
+	// requestAnimationFrame(tick);
 })(window, document);
+
+class Ball{
+	constructor(x, y, size, container){
+		// Assuming x, y define the center of the visible element. probably wrong but will fix later
+		this.x = x;
+		this.y = y;
+		this.size = size;
+
+		// Create HTML element and store
+		const newBall = document.createElement("div");
+		newBall.className = "ball";
+		this.element = newBall;
+
+		// Put the ball in the document
+		container.appendChild(newBall);
+	}
+
+	/**
+	 * Updates this ball's visuals based on internal data.
+	 * Generally should be called after all physics updates have been processed.
+	 */ 
+	updateVisual(){
+		this.element.style.left = `${this.x}px`;
+		this.element.style.bottom = `${this.y}px`;
+		this.element.style.width = `${this.size}px`; // TODO: SHOULD SCALE WITH WINDOW SIZE. LIKE A MULTIPLIER.
+		this.element.style.height = `${this.size}px`; // TODO: SHOULD SCALE WITH WINDOW SIZE. LIKE A MULTIPLIER.
+	}
+}
+
+function addBall(x, y){
+	const container = document.getElementById("ballContainer");
+	const newBall = new Ball(x, y, Math.floor(Math.random() * 100 + 100), container);
+	newBall.updateVisual();
+	balls.push(newBall);
+}
+
+// Tick "parameters"
+let prevTimestamp = undefined;
+const balls = [];
+
+function tick(timestamp){
+	const delta = !prevTimestamp ? 0 : timestamp - prevTimestamp;
+	prevTimestamp = timestamp;
+
+	// console.log(delta);
+
+	// Persist tick loop
+	requestAnimationFrame(tick);
+}
